d6a54bb94653b6f73941e05d8a2e4007e1c54e4e
Shrinkhla Rajpoot <shrinkhlarajpoot@gmail.com>
2023-03-27 12:57:35 +0530

    last-commit

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, getMe } = require("../controllers/auth");
+const { register, login, getMe, forgotPassword, resetPassword, logout } = require("../controllers/auth");
 const {protect} = require("../middleware/auth")
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me",protect,getMe);
+router.post("/forgotpassword",forgotPassword)
+router.put("/resetpassword/:resettoken",resetPassword)
+router.get("/logout",logout)
 module.exports = router;
